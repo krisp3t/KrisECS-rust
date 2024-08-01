7402496c6dd117de5c5da30258c182515ede7868
Kris Petrič <77499501+krisp3t@users.noreply.github.com>
2024-08-01 20:19:52 +0200

    Initial

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use bracket_lib::prelude::*;
+
+struct State {}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print(1, 1, "Hello Bracket World");
+    }
+}
+
+fn main() {}
