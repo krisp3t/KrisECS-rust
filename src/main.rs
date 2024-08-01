9ab547f80bda2df59a251309decbbe2da1ef7ec8
krisp3t <77499501+krisp3t@users.noreply.github.com>
2024-08-01 21:03:47 +0200

    GameState example

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,59 @@
 use bracket_lib::prelude::*;
 
-struct State {}
+enum GameMode {
+    Menu,
+    Playing,
+    End,
+}
+struct State {
+    mode: GameMode,
+}
+
+impl State {
+    fn new() -> Self {
+        Self {
+            mode: GameMode::Menu,
+        }
+    }
+
+    fn play(&mut self, ctx: &mut BTerm) {
+        self.mode = GameMode::End;
+    }
+
+    fn dead(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+    }
+
+    fn main_menu(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "Welcome to Simple BTerm Example");
+        ctx.print_centered(8, "(P) Play Game");
+        ctx.print_centered(9, "(Q) Quit");
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.mode = GameMode::Playing,
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
+    }
+}
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
         ctx.print(1, 1, "Hello Bracket World");
+        match self.mode {
+            GameMode::Menu => self.main_menu(ctx),
+            GameMode::Playing => self.play(ctx),
+            GameMode::End => self.dead(ctx),
+        }
     }
 }
 
-fn main() {}
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("Bracket Terminal Example - Simple")
+        .build()?;
+    main_loop(context, State::new())
+}
